baae82a3c07549d4c25cdb63c027dc2059b804e1
Bryan <bwbh87@gmail.com>
2016-10-02 15:45:30 +0800

    Cleaned up controller code

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('invitation');
+  },
+
   actions: {
+
+    saveInvitation(newInvitation) {
+      newInvitation.save().then((response) => this.controller.set('responseMessage', true));
+    },
+
     willTransition() {
+      this.controller.get('model').rollbackAttributes();
       this.controller.set('responseMessage', false);
     }
   }
