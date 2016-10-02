b22ea91f123db5115a53bf64ae7db5f0e8f55736
Bryan <bwbh87@gmail.com>
2016-10-02 17:39:33 +0800

    Added relationship for libraries, authors and books

--- app/models/library.js
+++ app/models/library.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
   phone: DS.attr('string'),
+  books: DS.hasMany('book'),
 
   isValidLibraryName: Ember.computed.notEmpty('name'),
   isNotValidLibraryName: Ember.computed.not('isValidLibraryName')
